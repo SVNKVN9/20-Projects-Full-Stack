e2e1828e11bc15a22135a0f5dbb7713823352540
SVNKVN9 <sundaynight.1919@gmail.com>
2024-02-29 10:14:19 +0700

    create simple page and api route

---
+++ 13.URL-shortener/app/api/url/route.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export async function GET(req: Request) {
+    const { searchParams } = new URL(req.url)
+    const id = searchParams.get('id')
+
+    return Response.json({ message: `Hello, ${id}` })
+}
+
+export async function POST(req: Request) {
+    const json = await req.json()
+    return Response.json({ message: `Hello, ${json.message}` })
+}
